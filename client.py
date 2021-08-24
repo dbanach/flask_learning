3a056e3a950c78f223b168fa9cd00f2f9e19142c
Daniel Banach <dbanach.tech@gmail.com>
2021-08-24 12:50:17 +0300

    added client and read X_test

---
+++ client.py
@@ -0,0 +1,19 @@
+import pickle
+import pandas as pd
+import numpy as np
+import requests
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    X_test = pd.read_csv('X_test.csv')
